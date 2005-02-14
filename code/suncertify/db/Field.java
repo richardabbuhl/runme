9461262c1a16825640b7eba224cefb72757f6fb2
rabbuhl <rabbuhl>
2005-02-14 16:06:02 +0000

    Initial revision.

---
+++ code/suncertify/db/Field.java
@@ -0,0 +1,35 @@
+package suncertify.db;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: RichardAbbuhl
+ * Date: Feb 14, 2005
+ * Time: 5:03:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Field {
+
+    private String name = new String();
+    private short length;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public short getLength() {
+        return length;
+    }
+
+    public void setLength(short length) {
+        this.length = length;
+    }
+
+    public void print() {
+        System.out.println("Name = " + name + ", length = " + length);
+    }
+
+}
