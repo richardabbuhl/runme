98f590d5434f2cb2b853a2c81fd2792c392d6a83
rabbuhl <rabbuhl>
2005-02-21 16:16:31 +0000

    Initial revision.

---
+++ code/suncertify/app/RunMe.java
@@ -0,0 +1,15 @@
+package suncertify.driver;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: RichardAbbuhl
+ * Date: Feb 21, 2005
+ * Time: 5:11:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class runme {
+
+    public static void main(String[] args) {
+        System.out.println("Runme!!");
+    }
+}
