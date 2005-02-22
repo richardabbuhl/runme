585409b3251e8fe0933b17f36256c3e812379768
rabbuhl <rabbuhl>
2005-02-22 13:05:59 +0000

    Initial revision.

---
+++ code/suncertify/rmi/RemoteData.java
@@ -0,0 +1,56 @@
+package suncertify.network;
+
+import suncertify.db.Data;
+import suncertify.db.RecordNotFoundException;
+import suncertify.db.DuplicateKeyException;
+
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: RichardAbbuhl
+ * Date: Feb 22, 2005
+ * Time: 1:58:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RemoteDataAdapter extends UnicastRemoteObject implements RemoteData {
+
+    protected Data adaptee;
+
+    public RemoteDataAdapter( Data adaptee ) throws RemoteException {
+       this.adaptee = adaptee;
+    }
+
+    public String[] read(int recNo) throws RecordNotFoundException {
+        return adaptee.read(recNo);
+    }
+
+    public void update(int recNo, String[] data, long lockCookie)
+            throws RecordNotFoundException, SecurityException {
+        adaptee.update(recNo, data, lockCookie);
+    }
+
+    public void delete(int recNo, long lockCookie)
+            throws RecordNotFoundException, SecurityException {
+        adaptee.delete(recNo, lockCookie);
+    }
+
+    public int[] find(String[] criteria) {
+        return adaptee.find(criteria);
+    }
+
+    public int create(String[] data) throws DuplicateKeyException {
+        return adaptee.create(data);
+    }
+
+    public long lock(int recNo) throws RecordNotFoundException {
+        return adaptee.lock(recNo);
+    }
+
+    public void unlock(int recNo, long cookie)
+            throws RecordNotFoundException, SecurityException {
+        adaptee.unlock(recNo, cookie);
+    }
+
+}
