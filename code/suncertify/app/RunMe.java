0852f58c9f0dfb8e7f059ffa41dcfc3143b356a9
rabbuhl <rabbuhl>
2005-02-23 15:49:26 +0000

    Updated.

--- code/suncertify/app/RunMe.java
+++ code/suncertify/app/RunMe.java
@@ -1,6 +1,7 @@
 package suncertify.app;
 
 import suncertify.network.RemoteDataAdapter;
+import suncertify.ui.RunMeFrame;
 
 import javax.swing.*;
 import java.awt.event.WindowEvent;
@@ -24,41 +25,6 @@ public class runme {
     private static final int STANDALONE_MODE = 2;
     private static final int CLIENT_MODE = 3;
 
-
-    private static String labelPrefix = "Number of button clicks: ";
-    private int numClicks = 0;
-
-    public Component createComponents() {
-        final JLabel label = new JLabel(labelPrefix + "0    ");
-
-        JButton button = new JButton("I'm a Swing button!");
-        button.setMnemonic('i');
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                numClicks++;
-                label.setText(labelPrefix + numClicks);
-            }
-        });
-        label.setLabelFor(button);
-
-        /*
-         * An easy way to put space between a top-level container
-         * and its contents is to put the contents in a JPanel
-         * that has an "empty" border.
-         */
-        JPanel pane = new JPanel();
-        pane.setBorder(BorderFactory.createEmptyBorder(30, //top
-                30, //left
-                10, //bottom
-                30) //right
-        );
-        pane.setLayout(new GridLayout(0, 1));
-        pane.add(button);
-        pane.add(label);
-
-        return pane;
-    }
-
     public void createServer(String hostName) {
         try {
             String name = "//" + hostName + "/RemoteData";
@@ -68,31 +34,6 @@ public class runme {
             e.printStackTrace();
         }
 
-    }
-
-    public void createUI() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-        } catch (Exception e) {
-        }
-
-        if (System.getSecurityManager() == null) {
-            System.setSecurityManager(new RMISecurityManager());
-        }
-
-        //Create the top-level container and add contents to it.
-        JFrame frame = new JFrame("SwingApplication");
-        Component contents = this.createComponents();
-        frame.getContentPane().add(contents, BorderLayout.CENTER);
-
-        //Finish setting up the frame, and show it.
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-        frame.pack();
-        frame.setVisible(true);
     }
 
     private static void showUsage() {
@@ -139,8 +80,8 @@ public class runme {
 
         } else if (mode == CLIENT_MODE) {
 
-            app.createUI();
-
+            RunMeFrame frame = new RunMeFrame();
+            frame.createUI();
         }
     }
 }
