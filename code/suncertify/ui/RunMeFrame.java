45b74547735c96e9ba22583dcc34b236ba71601b
rabbuhl <rabbuhl>
2005-02-25 16:19:08 +0000

    Updated.

--- code/suncertify/ui/RunMeFrame.java
+++ code/suncertify/ui/RunMeFrame.java
@@ -2,10 +2,7 @@ package suncertify.ui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,17 +16,89 @@ public class RunMeFrame extends JFrame {
     private static String labelPrefix = "Number of button clicks: ";
     private int numClicks = 0;
 
-    private void createMenu() {
-        JMenuBar mb = new JMenuBar();
-        JMenu fileMenu = new JMenu();
-        JMenuItem exitItem = new JMenuItem();
+    public JMenuBar createMenuBar() {
+        JMenuBar menuBar;
+        JMenu menu, submenu;
+        JMenuItem menuItem;
+        JRadioButtonMenuItem rbMenuItem;
+        JCheckBoxMenuItem cbMenuItem;
 
-        fileMenu.add("New...");
-        fileMenu.addSeparator();
-        fileMenu.add(exitItem);
+        //Create the menu bar.
+        menuBar = new JMenuBar();
 
-        mb.add(fileMenu);
-        setJMenuBar(mb);
+        //Build the first menu.
+        menu = new JMenu("A Menu");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        menuBar.add(menu);
+
+        //a group of JMenuItems
+        menuItem = new JMenuItem("A text-only menu item",
+                                 KeyEvent.VK_T);
+        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "This doesn't really do anything");
+        menu.add(menuItem);
+
+        //ImageIcon icon = createImageIcon("images/middle.gif");
+        //menuItem = new JMenuItem("Both text and icon", icon);
+        //menuItem.setMnemonic(KeyEvent.VK_B);
+        //menu.add(menuItem);
+
+        //menuItem = new JMenuItem(icon);
+        //menuItem.setMnemonic(KeyEvent.VK_D);
+        //menu.add(menuItem);
+
+        //a group of radio button menu items
+        menu.addSeparator();
+        ButtonGroup group = new ButtonGroup();
+
+        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
+        rbMenuItem.setSelected(true);
+        rbMenuItem.setMnemonic(KeyEvent.VK_R);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
+
+        rbMenuItem = new JRadioButtonMenuItem("Another one");
+        rbMenuItem.setMnemonic(KeyEvent.VK_O);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
+
+        //a group of check box menu items
+        menu.addSeparator();
+        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
+        cbMenuItem.setMnemonic(KeyEvent.VK_C);
+        menu.add(cbMenuItem);
+
+        cbMenuItem = new JCheckBoxMenuItem("Another one");
+        cbMenuItem.setMnemonic(KeyEvent.VK_H);
+        menu.add(cbMenuItem);
+
+        //a submenu
+        menu.addSeparator();
+        submenu = new JMenu("A submenu");
+        submenu.setMnemonic(KeyEvent.VK_S);
+
+        menuItem = new JMenuItem("An item in the submenu");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_2, ActionEvent.ALT_MASK));
+        submenu.add(menuItem);
+
+        menuItem = new JMenuItem("Another item");
+        submenu.add(menuItem);
+        menu.add(submenu);
+
+        //Build second menu in the menu bar.
+        menu = new JMenu("Another Menu");
+        menu.setMnemonic(KeyEvent.VK_N);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "This menu does nothing");
+        menuBar.add(menu);
+
+        return menuBar;
     }
 
     private Component createComponents() {
@@ -69,10 +138,8 @@ public class RunMeFrame extends JFrame {
         JFrame.setDefaultLookAndFeelDecorated(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Component contents = createComponents();
+        setJMenuBar(createMenuBar());
         getContentPane().add(contents, BorderLayout.CENTER);
-
-        // Create the menu bar.
-        createMenu();
 
         pack();
         setVisible(true);
