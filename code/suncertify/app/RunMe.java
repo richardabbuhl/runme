e8b004a607ccac16004224a4ff2cf8715d41ac54
rabbuhl <rabbuhl>
2005-02-25 15:58:29 +0000

    Updated.

--- code/suncertify/app/RunMe.java
+++ code/suncertify/app/RunMe.java
@@ -84,8 +84,13 @@ public class RunMe {
 
         } else if (mode == CLIENT_MODE) {
 
-            RunMeFrame frame = new RunMeFrame();
-            frame.createUI();
+            //Schedule a job for the event-dispatching thread:  creating and showing this application's GUI.
+            javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    RunMeFrame frame = new RunMeFrame();
+                    frame.createUI();
+                }
+            });
         }
     }
 }
