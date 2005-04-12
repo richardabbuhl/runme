b0b54b969f92938d3850058cebab6e8087e24bd0
rabbuhl <rabbuhl>
2005-04-12 19:48:19 +0000

    Updated.

--- code/suncertify/rmi/RemoteData.java
+++ code/suncertify/rmi/RemoteData.java
@@ -18,52 +18,117 @@ import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
 
 /**
- * Created by IntelliJ IDEA.
- * User: RichardAbbuhl
- * Date: Feb 22, 2005
- * Time: 1:58:17 PM
- * To change this template use File | Settings | File Templates.
+ * RemoteData implements the DB class as a remote interface to the Data class using the adopter / adoptee pattern.
+ *
+ * @author Richard Abbuhl
+ * @version 1.00
  */
 public class RemoteData extends UnicastRemoteObject implements DB {
 
+    /**
+     * Adoptee for the remote interface.
+     */
     protected Data adaptee;
 
+    /**
+     * Constructs a RemoteData object.
+     * @param adaptee Adaptee for the remote interface.
+     * @throws RemoteException thrown if a problem occurs during execution of the remote method call.
+     */
     public RemoteData(Data adaptee) throws RemoteException {
         super();
         this.adaptee = adaptee;
     }
 
+    /**
+     * Executes a remote call to reads a record from the file.
+     * @param recNo record from the file to be read.
+     * @return an array where each element is a record value.
+     * @throws RecordNotFoundException thrown if recNo cannot be found.
+     * @throws RemoteException thrown if a problem occurs during execution of the remote method call.
+     */
     public String[] read(int recNo)
             throws RecordNotFoundException, RemoteException {
         return adaptee.read(recNo);
     }
 
+    /**
+     * Executes a remote call to modify the fields of a record.
+     *
+     * @param recNo record number to be updated.
+     * @param data new values for this record.
+     * @param lockCookie cookie value that represents the lock.
+     * @throws RecordNotFoundException thrown if if recNo cannot be found.
+     * @throws SecurityException thrown if the record is not locked by cookie.
+     * @throws RemoteException thrown if a problem occurs during execution of the remote method call.
+     */
     public void update(int recNo, String[] data, long lockCookie)
             throws RecordNotFoundException, SecurityException, RemoteException {
         adaptee.update(recNo, data, lockCookie);
     }
 
+    /**
+     * Executes a remote call to delete a record.
+     *
+     * @param recNo record number to be deleted.
+     * @param lockCookie cookie value that represents the lock.
+     * @throws RecordNotFoundException thrown if recNo cannot be found.
+     * @throws SecurityException thrown if the record is not locked by cookie.
+     * @throws RemoteException thrown if a problem occurs during execution of the remote method call.
+     */
     public void delete(int recNo, long lockCookie)
             throws RecordNotFoundException, SecurityException, RemoteException {
         adaptee.delete(recNo, lockCookie);
     }
 
-    public int[] find(String[] criteria) {
+    /**
+     * Executes a remote call to returns an array of record numbers that match the specified criteria.
+     *
+     * @param criteria criteria used for matching records.
+     * @return an array of record numbers that match the specified criteria
+     * @throws RemoteException thrown if a problem occurs during execution of the remote method call.
+     */
+    public int[] find(String[] criteria) throws RemoteException {
         return adaptee.find(criteria);
     }
 
+    /**
+     * Executes a remote call to create a new record in the database.
+     *
+     * @param data values for this new record.
+     * @return the record number of the new record.
+     * @throws DuplicateKeyException thrown if the record cannot be created.
+     * @throws RemoteException thrown if a problem occurs during execution of the remote method call.
+     */
     public int create(String[] data)
             throws DuplicateKeyException, RemoteException {
         return adaptee.create(data);
     }
 
+    /**
+     * Executes a remote call to lock a record.
+     *
+     * @param recNo record number to be locked.
+     * @return cookie value that represents the lock.
+     * @throws RecordNotFoundException thrown if recNo cannot be found.
+     * @throws RemoteException thrown if a problem occurs during execution of the remote method call.
+     */
     public long lock(int recNo)
             throws RecordNotFoundException, RemoteException {
         return adaptee.lock(recNo);
     }
 
+    /**
+     * Executes a remote call to releases the lock on a record.
+     *
+     * @param recNo record number to be unlocked.
+     * @param cookie cookie value that represents the lock.
+     * @throws RecordNotFoundException
+     * @throws SecurityException thrown if the record is not locked by cookie.
+     * @throws RemoteException thrown if a problem occurs during execution of the remote method call.
+     */
     public void unlock(int recNo, long cookie)
-            throws RecordNotFoundException, SecurityException {
+            throws RecordNotFoundException, SecurityException, RemoteException {
         adaptee.unlock(recNo, cookie);
     }
 
