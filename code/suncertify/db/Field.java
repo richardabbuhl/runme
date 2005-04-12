41d50b022687fe056e1f3ae7969cb38b2887e23b
rabbuhl <rabbuhl>
2005-04-12 19:04:26 +0000

    Updated.

--- code/suncertify/db/Field.java
+++ code/suncertify/db/Field.java
@@ -12,30 +12,57 @@ package suncertify.db;
 /**
  * Field represent a column in the customer's non-relational database file.  A collection of fields
  * represents the databse schema.
- * @version 1.00, April 12, 2005
+ *
  * @author Richard Abbuhl
+ * @version 1.00, April 12, 2005
  */
 public class Field {
 
+    /**
+     * The name of the column in the database.
+     */
     private String name = new String();
+
+    /**
+     * The length of the column in the database.
+     */
     private short length;
 
+    /**
+     * Gets the name of the column.
+     * @return the name of the column.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the column.
+     * @param name the name of the column.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the length of the column.
+     * @return the length of the column.
+     */
     public short getLength() {
         return length;
     }
 
+    /**
+     * Sets the length of the column.
+     * @param length the length of the column.
+     */
     public void setLength(short length) {
         this.length = length;
     }
 
+    /**
+     * Dumps the value of all attributes.
+     */
     public void dump() {
         System.out.println("Name=" + name + ",length=" + length);
     }
